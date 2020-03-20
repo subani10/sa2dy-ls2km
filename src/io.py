ea9ae5d6874c25411c11eab99ddea6f355145d04
leelashah <39598836+leelashah@users.noreply.github.com>
2020-03-20 08:03:23 -0400

    Update io.py
    Updated to include functions for unpacking, loading, and examining the attributes of data.

--- src/io.py
+++ src/io.py
@@ -1,4 +1,38 @@
 # -*- coding: utf-8 -*-
 # -*- mode: python -*-
-"""Functions for file IO"""
+
+"""packages to import"""
 from __future__ import print_function, division, absolute_import
+import os
+import h5py
+
+"""Functions for file IO"""
+
+def data_unpack(data):
+    """
+    This function unpacks the given .gz file. The argument is the file name (ex: s1.tar.gz).
+    """
+    tar -zxvf data
+    
+def load_data(subject):
+    """
+    This function loads the data collected from a given subject (s1-s7) 
+    from the data set fcx-3 from crcns.org, and then saves the data under the variable
+    named "data." The argument is the subject name (ex: "s1").
+    """
+    path = os.path.join("data", subject, "data_primary.mat")
+    data= h5py.File(path, mode='r')
+    return data
+def data_attributes(subject):
+    """
+    This function loads the data collected froma given subject, using the function load_data
+    as described above. The module then returns the file within the data set, and the shape
+    and data type of this file. The file "data_primary.mat" that is downloaded for each
+    subject contains only one file, so the list of the data keys only returns one object.
+    """
+    data = load_data(subject)
+    file = list(data.keys())
+    dset = data[file[0]]
+    shape = dset.shape
+    data_type = dset.dtype
+    return file,dset,shape,data_type
